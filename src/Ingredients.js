38ab5778bb2799923ef4c622f5acf93873a498e0
taegorov <taegorov@gmail.com>
2021-05-04 14:23:42 -0700

    updates accordion to have drop down selections, adds submit button

--- src/Ingredients.js
+++ src/Ingredients.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardColumns, Accordion } from 'react-bootstrap';
+import { Card, CardColumns, Accordion, Form, Button } from 'react-bootstrap';
 
 
 class Ingredients extends React.Component {
@@ -15,57 +15,75 @@ class Ingredients extends React.Component {
     this.setState({ clickedIngredient: true })
   }
 
+  handleSubmit = (event) => {
+    this.props.filterHorns(parseInt(event.target.value));
+  }
+
 
   render() {
     return (
       <>
-        <Accordion defaultActiveKey="1">
+        <Form>
 
-          <Card>
-            <Accordion.Toggle as={Card.Header} eventKey="0">
-              Click to Choose Protein
+          <Accordion defaultActiveKey="1">
+
+            <Card>
+              <Accordion.Toggle as={Card.Header} eventKey="0">
+                Click to Choose Protein
             </Accordion.Toggle>
-            <Accordion.Collapse eventKey="0">
-              <CardColumns>
-                <Card
-                  onClick={this.addIngredient}>Chicken {this.state.clickedIngredient}</Card>
-                <Card>Beef</Card>
-                <Card>Pork</Card>
-              </CardColumns>
-            </Accordion.Collapse>
-          </Card>
+              <Accordion.Collapse eventKey="0">
+                <Form.Group proteinId="proteinForm.Select">
+                  <Form.Control as="select">
+                    <option>...</option>
+                    <option>Chicken</option>
+                    <option>Beef</option>
+                    <option>Mutton</option>
+                    <option>Fish</option>
+                  </Form.Control>
+                </Form.Group>
+              </Accordion.Collapse>
+            </Card>
 
-        </Accordion>
+          </Accordion>
 
-        <Accordion defaultActiveKey="1">
-          <Card>
-            <Accordion.Toggle as={Card.Header} eventKey="0">
-              Click to Choose Veggies
+          <Accordion defaultActiveKey="1">
+            <Card>
+              <Accordion.Toggle as={Card.Header} eventKey="0">
+                Click to Choose Veggies
             </Accordion.Toggle>
-            <Accordion.Collapse eventKey="0">
-              <CardColumns>
-                <Card>Lettuce</Card>
-                <Card>Onion</Card>
-                <Card>Tomato</Card>
-              </CardColumns>
-            </Accordion.Collapse>
-          </Card>
-        </Accordion>
+              <Accordion.Collapse eventKey="0">
+                <Form.Group veggiesId="veggiesForm.Select">
+                  <Form.Control as="select" multiple>
+                    <option>Lettuce</option>
+                    <option>Onion</option>
+                    <option>Tomato</option>
+                    <option>Cilantro</option>
+                  </Form.Control>
+                </Form.Group>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>
 
-        <Accordion defaultActiveKey="1">
-          <Card>
-            <Accordion.Toggle as={Card.Header} eventKey="0">
-              Click to Choose Cheese
+          <Accordion defaultActiveKey="1">
+            <Card>
+              <Accordion.Toggle as={Card.Header} eventKey="0">
+                Click to Choose Cheese
             </Accordion.Toggle>
-            <Accordion.Collapse eventKey="0">
-              <CardColumns>
-                <Card>Cheddar</Card>
-                <Card>Cotija</Card>
-                <Card>Goat</Card>
-              </CardColumns>
-            </Accordion.Collapse>
-          </Card>
-        </Accordion>
+              <Accordion.Collapse eventKey="0">
+                <Form.Group proteinId="proteinForm.Select">
+                  <Form.Control as="select">
+                    <option>...</option>
+                    <option>Cheddar</option>
+                    <option>Mojito or whatever Skyler said</option>
+                    <option>Sharp Cheddar</option>
+                    <option>Super Sharp Cheddar</option>
+                  </Form.Control>
+                </Form.Group>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>
+          <Button variant="dark" onClick={this.handleSubmit}>Build Recipe</Button>
+        </Form>
 
       </>
     )
